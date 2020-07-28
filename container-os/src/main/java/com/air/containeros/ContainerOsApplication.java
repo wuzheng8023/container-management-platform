581bf9d99aae0895c193db76b92cc8121c4f1cc2
wuzheng <1345381396@qq.com>
2020-07-28 16:57:54 +0800

    初始化完成

---
+++ container-os/src/main/java/com/air/containeros/ContainerOsApplication.java
@@ -0,0 +1,13 @@
+package com.air.containeros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerOsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContainerOsApplication.class, args);
+    }
+
+}
